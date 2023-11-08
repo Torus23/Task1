352c9c1874a04b8b940e0189a5724830554b862e
Logan Bull <107020194+Torus23@users.noreply.github.com>
2023-11-08 16:55:44 +1300

    Update Dbconnect.php

--- Include/Dbconnect.php
+++ Include/Dbconnect.php
@@ -2,7 +2,7 @@
     require "../dbconfig.php";
     $connection = mysqli_init();
     mysqli_ssl_set($connection,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-    mysqli_real_connect($connection,  $servername, $username, $password, $dbname,  $port);
+    mysqli_real_connect($connection,  $servername, $username, $password, $dbname,  $port, MYSQLI_CLIENT_SSL);
     if (mysqli_connect_errno()) {
     die('Failed to connect to MySQL: '.mysqli_connect_error()); }
 
